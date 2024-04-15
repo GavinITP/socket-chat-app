6e27a54c9dcf4a74e8cebb869109a8cd5a9973b2
GavinITP <86824319+GavinITP@users.noreply.github.com>
2024-04-15 13:38:16 +0700

    feat: connect to mongo db

--- backend/index.ts
+++ backend/index.ts
@@ -1,32 +1,48 @@
+// libraries
 import express from "express";
 import dotenv from "dotenv";
-import { chats } from "./data/data";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
-dotenv.config({ path: ".env" });
+// mock data
+import { chats } from "./data/data";
+
+// config
+import connectDB from "./config/db";
+
+// routes
+
+// set up
+dotenv.config({ path: "config/.env" });
+connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// middleware
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
 
+// routes
 app.get("/", (req, res) => {
   res.send("Hello");
 });
-
 app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
-
 app.get("/api/chat/:id", (req, res) => {
   const singleChat = chats.find((c) => c._id === req.params.id);
   res.send(singleChat);
 });
 
+// listen
 const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => {
-  console.log("Server is listening on port", PORT);
+const server = app.listen(PORT, () => {
+  console.log("Server is running on port:", PORT);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${(err as Error).message}`);
+  server.close(() => process.exit(1));
 });
